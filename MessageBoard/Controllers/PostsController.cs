9f05a5115a7008286016205d9bd101d03b4a5463
Vitalik Dydko <vitalikdudko127@gmail.com>
2015-06-22 22:40:01 +0300

    add MessageBoard projeckt

---
+++ MessageBoard/Controllers/PostsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MessageBoard.Models;
+
+namespace MessageBoard.Controllers
+{
+    public class PostsController : ApiController
+    {
+        private MessageBoardContext _ctx;
+        public PostsController()
+        {
+            this._ctx = new MessageBoardContext();
+        }
+
+        // GET api/<controller>
+        public IEnumerable<Post> Get()
+        {
+            return this._ctx.Posts.OrderByDescending(x => x.DatePosted).ToList();
+        }
+    }
+}
---
+++ MessageBoard/Models/MessageBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MessageBoard.Models
+{
+    public class MessageBoardContext : DbContext
+    {
+        public MessageBoardContext():base("DefaultConnection")
+        { }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Username { get; set; }
+        public DateTime DatePosted { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Username { get; set; }
+        public DateTime DatePosted { get; set; }
+        public virtual Post ParentPost { get; set; }
+    }
+}
